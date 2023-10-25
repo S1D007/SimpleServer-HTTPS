1ba5f530b4e08b87025b12596d9498c841210874
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-10-25 11:39:23 +0530

    Update sshs.sh

--- sshs.sh
+++ sshs.sh
@@ -1,86 +1,26 @@
 #!/bin/bash
 
-# Function to revert changes
-revert_changes() {
-  echo "Reverting changes..."
-  # Revert the system to the initial state
-  # Add necessary commands here to revert changes made by the script
-  echo "Reverted successfully."
-}
-
-# Function to display help
-display_help() {
-  echo "This script automates the setup and deployment process for your project."
-  echo "Usage: bash script_name.sh"
-  echo "Options:"
-  echo "  -h, --help     Display this help message."
-  echo "  -r, --revert   Revert changes made by this script."
-  exit 0
-}
-
-# Error handling function
-handle_error() {
-  echo "An error occurred. Reverting changes..."
-  revert_changes
-  exit 1
-}
-
-# Trap errors
-trap 'handle_error' ERR
-
-# Display help if requested
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-  display_help
-fi
-
-# Function to display a loading animation
-show_spinner() {
-  local -r pid="$1"
-  local -r delay='0.75'
-  local spinstr='|/-\'
-  while [ "$(ps a | awk '{print $1}' | grep "$pid")" ]; do
-    local temp=${spinstr#?}
-    printf " [%c]  " "$spinstr"
-    local spinstr=$temp${spinstr%"$temp"}
-    sleep "$delay"
-    printf "\b\b\b\b\b\b"
-  done
-  printf "    \b\b\b\b"
-}
-
 # Update the system
-echo "Updating the system..."
-sudo apt-get update &> /dev/null &
-show_spinner "$!"
+sudo apt-get update
 
 # Pull your repository from the specified URL
 echo "Enter the repository URL:"
-read -r repo_url
+read repo_url
 repo_directory=$(basename "$repo_url" .git)
-echo "Downloading the repository..."
-git clone "$repo_url" "$repo_directory" &> /dev/null &
-show_spinner "$!"
+git clone "$repo_url" "$repo_directory"
 
 # Install NVM
-echo "Installing NVM..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash &> /dev/null &
-show_spinner "$!"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# Load NVM and install Node.js
+# Load NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install Node.js 18
-echo "Installing Node.js 18..."
-nvm install 18 &> /dev/null &
-show_spinner "$!"
-
-# Use Node.js 18
-nvm use 18
+nvm install 18
 
 # Install npm packages
 cd "$repo_directory"
-echo "Installing npm packages..."
 npm install
 
 # Initialize an array to store files to be copied
@@ -114,21 +54,8 @@ for ((i = 0; i < ${#files_to_copy[@]}; i+=2)); do
   cp -r "$source" "$destination"
 done
 
-# Check if the project is a TypeScript project
-echo "Is this a TypeScript project? (yes/no)"
-read is_typescript
-
-if [ "$is_typescript" == "yes" ]; then
-  # Ask for the build script for TypeScript
-  echo "Enter the script name (ex: npm run build) for building the TypeScript project:"
-  read build_script
-fi
-
-# Build the project based on the user input
-if [ "$is_typescript" == "yes" ]; then
-  echo "Building TypeScript project using script: $build_script"
-  npm run "$build_script"
-fi
+# Build the project
+npm run build
 
 # Install PM2
 npm install pm2 -g
